3ab0016b017ac29000f392b2d73e02b1256ca02f
cncz42 <zwickson42@gmail.com>
2024-10-12 20:13:16 -0700

    More camera work

--- src/app/camera-video/camera-video.component.ts
+++ src/app/camera-video/camera-video.component.ts
@@ -1,7 +1,8 @@
 import {ChangeDetectionStrategy, Component, Input, NgModule} from "@angular/core";
 import {CameraButtonComponent} from './camera-button/camera-button.component';
 import {WebcamModule} from 'ngx-webcam';
-
+import {Subject, Observable} from 'rxjs';
+import {WebcamImage, WebcamInitError, WebcamUtil} from 'ngx-webcam';
 
 @Component({
   selector: "app-camera-video",
@@ -15,4 +16,25 @@ import {WebcamModule} from 'ngx-webcam';
   standalone: true
 })
 
-export class CameraVideoComponent {}
+export class CameraVideoComponent {
+  private trigger: Subject<void> = new Subject<void>();
+  public webcamImage: WebcamImage = null;
+  public videoOptions: MediaTrackConstraints = {
+    width: {ideal: 393},
+    height: {ideal: 852}
+  };
+
+
+  public triggerSnapshot(): void {
+    this.trigger.next();
+  }
+
+  public get triggerObservable(): Observable<void> {
+    return this.trigger.asObservable();
+  }
+
+  public handleImage(webcamImage: WebcamImage): void {
+    console.info('received webcam image', webcamImage);
+    this.webcamImage = webcamImage;
+  }
+}
